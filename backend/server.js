2867dcb6be85517c2807d54b35d472be5d82dc47
sean-charles-allen <seanallen555@protonmail.com>
2025-03-04 15:24:13 +1300

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json()); // Middleware to parse JSON
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('MERN App Backend is Running!');
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
